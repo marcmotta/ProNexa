bfc06c2f2e45c21f51c8b45902530763c44adf88
Marcos Mota <60157172+marcmotta@users.noreply.github.com>
2025-07-27 12:10:21 +0700

    Add pronexa.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ internal/pronexa/pronexa.go
@@ -0,0 +1,36 @@
+// internal/pronexa/pronexa.go
+package pronexa
+
+import (
+	"log"
+	"os"
+)
+
+type App struct {
+	verbose bool
+	logger  *log.Logger
+}
+
+func NewApp(verbose bool) *App {
+	app := &App{
+		verbose: verbose,
+		logger:  log.New(os.Stdout, "", log.LstdFlags),
+	}
+
+	if verbose {
+		app.logger.SetPrefix("[DEBUG] ")
+	} else {
+		app.logger.SetPrefix("[INFO] ")
+	}
+
+	return app
+}
+
+func (a *App) Run() error {
+	a.logger.Printf("Starting ProNexa processing")
+
+	// Add your implementation here
+
+	a.logger.Println("Processing completed successfully")
+	return nil
+}
